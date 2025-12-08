783be538d205f2304dc6be784592eb33d1ea3b92
Tyuiu.GolovanovDO@outlook.com <Tyuiu.GolovanovDO@outlook.com>
2025-12-08 18:00:17 +0500

    t6

--- Tyuiu.GolovanovDO.Sprint6.Task6.V1.Lib/DataService.cs
+++ Tyuiu.GolovanovDO.Sprint6.Task6.V1.Lib/DataService.cs
@@ -5,22 +5,23 @@ namespace Tyuiu.GolovanovDO.Sprint6.Task6.V1.Lib
     {
         public string CollectTextFromFile(string path)
         {
-            
-            string firstWord = "";
+            string[] lines = File.ReadAllLines(path);
+            List<string> firstWords = new List<string>();
 
-            string[] str = File.ReadAllLines(path);
-
-
-
-
-            foreach (string line in str)
+            foreach (string line in lines)
             {
-                firstWord = firstWord  + line.Split(' ').First() + " ";
-                
+                if (!string.IsNullOrWhiteSpace(line))
+                {
+                    // Убираем пробелы в начале/конце строки и игнорируем пустые элементы
+                    string[] words = line.Trim().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                    if (words.Length > 0)
+                    {
+                        firstWords.Add(words[0]);
+                    }
+                }
             }
-            firstWord = firstWord.Substring(0, firstWord.Length -1);
-            return firstWord;
 
+            return string.Join(" ", firstWords);
         }
     }
 }
